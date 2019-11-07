86064bf76bd826da49d9da1b38f517d0f4c572ea
larissa-coder <56737062+larissa-coder@users.noreply.github.com>
2019-11-07 11:23:38 -0500

    Update Analyze ACTH.R

--- scripts/Analyze ACTH.R
+++ scripts/Analyze ACTH.R
@@ -2,13 +2,16 @@
 
 ## Install necessary packages
 
+
 install.packages("ggplot2")
 library(ggplot2)
 
+install.packages("scatterplot3d")
+library(scatterplot3d)
+
 ## Read data
-IBS1 <- read.csv("data/RobinsonEtAl_Sup1.csv", header = TRUE)
-head(IBS1)
-write.csv(IBS1, "data_output/output.csv")
+IBS1 <- read.csv("RobinsonEtAl_Sup1.csv", header = TRUE)
+
 ##  Single Regressions for BMI vs. ACTH
 ##  Data was obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
 ##  https://statquest.org/2017/10/30/statquest-multiple-regression-in-r/
@@ -16,21 +19,29 @@ write.csv(IBS1, "data_output/output.csv")
 ##  http://r-statistics.co/Linear-Regression.html
 
 
-## Single Regression Test
-single.regression <- lm(BMI~ACTH, data=ISB1)
-print(single.regression)
+single.regression <- lm(BMI ~ SerumCortisol, data=IBS1)
 summary(single.regression)
-## Scatterplots
-## https://www.statmethods.net/graphs/scatterplot.html
 
-ggplot(IBS1, aes(x=BMI, y=ACTH)) +
-geom_point() +    
-geom_smooth(method=lm) 
+single.regression <- lm(BMI ~ CRP, data=IBS1)
+summary(single.regression)
+
+single.regression <- lm(BMI ~ ESR, data=IBS1)
+summary(single.regression)
+
+single.regression <- lm(BMI ~ PlateletCount, data=IBS1)
+summary(single.regression)
+
+single.regression <- lm(BMI ~ IgA, data=IBS1)
+summary(single.regression)
+
+single.regression <- lm(BMI ~ ACTH, data=IBS1)
+summary(single.regression)
 
 #BMI, ACTH
 single.regression <- lm(BMI ~ ACTH, data=IBS1)
 print(single.regression)
 df<-na.omit(data)
+
 ggplot(IBS1, aes(x=BMI, y=ACTH)) +
   geom_point() +    
   geom_smooth(method=lm)
@@ -51,7 +62,7 @@ boxplot(ACTH ~ IBS.subtype, data = IBS1, main="ACTH by IBS1 subtype",
 )
 
 png("fig_output/ACTH_boxplot.png")
-Basophils_boxplot <- boxplot(Basophils ~ IBS.subtype, data = IBS1, main="ACTH by IBS1 subtype", 
+ACTH_boxplot <- boxplot(ACTH ~ IBS.subtype, data = IBS1, main="ACTH by IBS1 subtype", 
                              xlab = "IBS1.subtype", ylab = "ACTH"
 )
 print(ACTH_boxplot)
